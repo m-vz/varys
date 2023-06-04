5e23831611a79cd65f8a08f6a5d21c159361a793
milan <milan@milan.wtf>
2023-06-04 23:53:19 +0200

    feat: assistant enum refactor: move siri to its own module

--- src/assistant.rs
+++ src/assistant.rs
@@ -1,9 +1,7 @@
-use crate::cli::interact;
-use crate::cli::key_type::KeyType;
-use crate::speak::Speaker;
+pub mod siri;
+
+use crate::assistant::siri::Siri;
 use crate::{cli, speak};
-use colored::Colorize;
-use log::info;
 use thiserror::Error;
 
 #[derive(Error, Debug)]
@@ -14,68 +12,26 @@ pub enum Error {
     Speaker(#[from] speak::Error),
 }
 
+pub enum Assistant {
+    Siri(Siri),
+    Unavailable,
+}
+
+impl From<&str> for Assistant {
+    fn from(value: &str) -> Self {
+        match &value.to_lowercase()[..] {
+            "siri" => Assistant::Siri(Siri {}),
+            _ => Assistant::Unavailable,
+        }
+    }
+}
+
+impl Default for Assistant {
+    fn default() -> Self {
+        Assistant::Siri(Siri {})
+    }
+}
+
 pub trait VoiceAssistant {
     fn setup(&self) -> Result<(), Error>;
-}
-
-pub struct Siri {}
-
-impl Siri {
-    pub const PREMIUM_VOICES: &'static [&'static str] =
-        &["Ava", "Karen", "Jamie", "Matilda", "Serena", "Zoe"];
-}
-
-impl VoiceAssistant for Siri {
-    fn setup(&self) -> Result<(), Error> {
-        info!("Starting Siri setup...");
-
-        let mut speaker = Speaker::new()?;
-
-        let voice = interact::user_input(
-            &format!(
-                "Choose the voice to set up (The highest quality voices on macOS are {}):",
-                Siri::PREMIUM_VOICES.join(", ")
-            ),
-            |i| speaker.set_voice(i).is_ok(),
-            "Voice not found, enter a voice that can be used on this system:",
-        )?;
-        println!("Setting up Siri for {}", voice);
-        interact::user_confirmation(
-            "This requires a number of sentences to be said. To continue, press",
-        )?;
-        interact::user_confirmation(
-            "Make sure your iOS device is close enough to this computer to hear it.",
-        )?;
-        interact::user_confirmation(&format!(
-            "On your iOS device, go to {} > {} and enable {} (you might have to disable it first)",
-            "Settings".bright_blue(),
-            "Siri & Search".bright_blue(),
-            "Listen for \"Hey Siri\"".bright_blue()
-        ))?;
-        interact::user_confirmation(&format!(
-            "The sentences will now be said. Press {} on your device and then",
-            "Continue".bright_blue()
-        ))?;
-        for sentence in [
-            "Hey Siri",
-            "Hey Siri. Send a message.",
-            "Hey Siri. How's the weather today?",
-            "Hey Siri. Set a timer for three minutes.",
-            "Hey Siri. Play some music.",
-        ] {
-            loop {
-                speaker.say(sentence, true)?;
-                if interact::user_choice(
-                    "Confirm that Siri recognised the sentence or repeat it",
-                    &[KeyType::Enter, KeyType::Key('r')],
-                )? == KeyType::Enter
-                {
-                    break;
-                }
-            }
-        }
-        println!("Finished setting up Siri");
-
-        Ok(())
-    }
 }
---
+++ src/assistant/siri.rs
@@ -0,0 +1,68 @@
+use crate::assistant::{Error, VoiceAssistant};
+use crate::cli::interact;
+use crate::cli::key_type::KeyType;
+use crate::speak::Speaker;
+use colored::Colorize;
+use log::info;
+
+pub struct Siri {}
+
+impl Siri {
+    pub const PREMIUM_VOICES: &'static [&'static str] =
+        &["Ava", "Karen", "Jamie", "Matilda", "Serena", "Zoe"];
+}
+
+impl VoiceAssistant for Siri {
+    fn setup(&self) -> Result<(), Error> {
+        info!("Starting Siri setup...");
+
+        let mut speaker = Speaker::new()?;
+
+        let voice = interact::user_input(
+            &format!(
+                "Choose the voice to set up (The highest quality voices on macOS are {}):",
+                Siri::PREMIUM_VOICES.join(", ")
+            ),
+            |i| speaker.set_voice(i).is_ok(),
+            "Voice not found, enter a voice that can be used on this system:",
+        )?;
+        println!("Setting up Siri for {}", voice);
+        interact::user_confirmation(
+            "This requires a number of sentences to be said. To continue, press",
+        )?;
+        interact::user_confirmation(
+            "Make sure your iOS device is close enough to this computer to hear it.",
+        )?;
+        interact::user_confirmation(&format!(
+            "On your iOS device, go to {} > {} and enable {} (you might have to disable it first)",
+            "Settings".bright_blue(),
+            "Siri & Search".bright_blue(),
+            "Listen for \"Hey Siri\"".bright_blue()
+        ))?;
+        interact::user_confirmation(&format!(
+            "The sentences will now be said. Press {} on your device and then",
+            "Continue".bright_blue()
+        ))?;
+        for sentence in [
+            "Hey Siri",
+            "Hey Siri. Send a message.",
+            "Hey Siri. How's the weather today?",
+            "Hey Siri. Set a timer for three minutes.",
+            "Hey Siri. Play some music.",
+        ] {
+            loop {
+                speaker.say(sentence, true)?;
+                if interact::user_choice(
+                    "Confirm that Siri recognised the sentence or repeat it",
+                    &[KeyType::Enter, KeyType::Key('r')],
+                )? == KeyType::Enter
+                {
+                    break;
+                }
+            }
+        }
+        println!("Finished setting up Siri");
+
+        Ok(())
+    }
+}
