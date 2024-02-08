544ec1487964528c56d954083a8c4e0489af009d
milan <milan@milan.wtf>
2024-02-08 13:19:49 +0100

    feat: add numeric batcher

--- varys-analysis/src/ml/data.rs
+++ varys-analysis/src/ml/data.rs
@@ -1,6 +1,8 @@
 use burn::tensor::backend::Backend;
+use burn::tensor::{Data, ElementConversion, Int, Tensor};
 
 pub mod binary;
+pub mod numeric;
 
 pub struct TrafficTraceBatcher<B: Backend> {
     device: B::Device,
@@ -10,4 +12,15 @@ impl<B: Backend> TrafficTraceBatcher<B> {
     pub fn new(device: B::Device) -> Self {
         Self { device }
     }
+}
+
+fn build_targets<B: Backend>(labels: Vec<u8>, device: &B::Device) -> Tensor<B, 1, Int> {
+    let targets = labels
+        .iter()
+        // in this step we convert each item to the backend element type
+        .map(|&label| Data::from([(label as i64).elem()]))
+        .map(|data| Tensor::<B, 1, Int>::from_data(data, device))
+        .collect();
+
+    Tensor::cat(targets, 0).to_device(device)
 }
--- varys-analysis/src/ml/data/numeric.rs
+++ varys-analysis/src/ml/data/numeric.rs
@@ -5,25 +5,25 @@ use serde::{Deserialize, Serialize};
 
 use crate::ml::data;
 use crate::ml::data::TrafficTraceBatcher;
-use crate::trace::BinaryTrafficTrace;
+use crate::trace::NumericTrafficTrace;
 
 #[derive(Deserialize, Serialize, Debug, Clone)]
-pub struct BinaryTraceItem {
-    pub trace: BinaryTrafficTrace,
+pub struct NumericTraceItem {
+    pub trace: NumericTrafficTrace,
     pub label: u8,
 }
 
 #[derive(Clone, Debug)]
-pub struct BinaryBatch<B: Backend> {
+pub struct NumericBatch<B: Backend> {
     pub traces: Tensor<B, 2>,
     pub targets: Tensor<B, 1, Int>,
 }
 
-impl<B: Backend> Batcher<BinaryTraceItem, BinaryBatch<B>> for TrafficTraceBatcher<B> {
-    fn batch(&self, items: Vec<BinaryTraceItem>) -> BinaryBatch<B> {
+impl<B: Backend> Batcher<NumericTraceItem, NumericBatch<B>> for TrafficTraceBatcher<B> {
+    fn batch(&self, items: Vec<NumericTraceItem>) -> NumericBatch<B> {
         let traces = items
             .iter()
-            .map(|item| Data::<bool, 1>::from(item.trace.0.as_slice()))
+            .map(|item| Data::<i32, 1>::from(item.trace.0.as_slice()))
             // in this step we convert all data to the backend type
             .map(|data| Tensor::<B, 1>::from_data(data.convert(), &self.device))
             .map(|tensor| {
@@ -33,7 +33,7 @@ impl<B: Backend> Batcher<BinaryTraceItem, BinaryBatch<B>> for TrafficTraceBatche
             .collect();
         let traces = Tensor::cat(traces, 0).to_device(&self.device);
 
-        BinaryBatch {
+        NumericBatch {
             traces,
             targets: data::build_targets(
                 items.into_iter().map(|item| item.label).collect(),
