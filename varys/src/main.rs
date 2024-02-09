a69e4325718263d8c00e1c1214a20512ce3a9e3b
milan <milan@milan.wtf>
2024-02-09 17:29:19 +0100

    feat: correctly log error in main function

--- varys/src/main.rs
+++ varys/src/main.rs
@@ -1,8 +1,18 @@
-use varys::cli;
+use log::error;
+use std::str::FromStr;
+
 use varys::error::Error;
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
+    main_fallible().await.map_err(|error| {
+        error!("{error}");
+
+        error
+    })
+}
+
+async fn main_fallible() -> Result<(), Error> {
     dotenvy::dotenv().map_err(|error| Error::Dotenv(error.to_string()))?;
     pretty_env_logger::init();
 
